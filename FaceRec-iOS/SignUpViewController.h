cee66de2c77ae53fcf7f257c38c3a85ef0d98f5f
Charles Wang <cmw252@cornell.edu>
2014-02-25 01:54:23 -0500

    Updated Sign Up Controller

--- FaceRec-iOS/SignUpViewController.h
+++ FaceRec-iOS/SignUpViewController.h
@@ -2,22 +2,23 @@
 //  SignUpViewController.h
 //  FaceRec-iOS
 //
-//  Created by Charles Wang on 2/15/14.
+//  Created by Charles Wang on 2/25/14.
 //  Copyright (c) 2014 Charles Wang. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "FaceRecServer.h"
 
-@interface SignUpViewController : UIViewController <UITextFieldDelegate, NSURLConnectionDelegate, UIAlertViewDelegate>
+@interface SignUpViewController : UITableViewController <NSURLConnectionDelegate, UIAlertViewDelegate, UITextFieldDelegate>
 
-@property (strong, nonatomic) IBOutlet UIScrollView *scroller;
-@property (strong, nonatomic) IBOutlet UITextField *firstName_field;
-@property (strong, nonatomic) IBOutlet UITextField *lastName_field;
-@property (strong, nonatomic) IBOutlet UITextField *email_field;
-@property (strong, nonatomic) IBOutlet UITextField *username_field;
-@property (strong, nonatomic) IBOutlet UITextField *password_field;
-@property (strong, nonatomic) IBOutlet UITextField *cornfirmPassword_field;
+@property (strong, nonatomic) UITextField *firstName_field;
+@property (strong, nonatomic) UITextField *lastName_field;
+@property (strong, nonatomic) UITextField *email_field;
+@property (strong, nonatomic) UITextField *username_field;
+@property (strong, nonatomic) UITextField *password_field;
+@property (strong, nonatomic) UITextField *confirmPassword_field;
+@property (strong, nonatomic) UIButton *signupButton;
+
 @property (strong, nonatomic) NSMutableData *responseData;
 @property (strong, nonatomic) NSHTTPURLResponse *headerResponse;
 @property (strong, nonatomic) NSDictionary *jsonResponse;
--- FaceRec-iOS/SignUpViewController.m
+++ FaceRec-iOS/SignUpViewController.m
@@ -2,7 +2,7 @@
 //  SignUpViewController.m
 //  FaceRec-iOS
 //
-//  Created by Charles Wang on 2/15/14.
+//  Created by Charles Wang on 2/25/14.
 //  Copyright (c) 2014 Charles Wang. All rights reserved.
 //
 
@@ -14,15 +14,12 @@
 
 @implementation SignUpViewController
 
-@synthesize responseData, headerResponse, jsonResponse;
+@synthesize username_field, email_field, password_field, confirmPassword_field, firstName_field, lastName_field, signupButton;
+@synthesize alertView, isSuccess, responseData, jsonResponse, headerResponse;
 
-@synthesize scroller, username_field, email_field, password_field, cornfirmPassword_field, firstName_field, lastName_field;
-
-@synthesize alertView, isSuccess;
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)initWithStyle:(UITableViewStyle)style
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithStyle:style];
     if (self) {
         // Custom initialization
     }
@@ -32,13 +29,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [scroller setScrollEnabled:YES];
-    [scroller setContentSize:CGSizeMake(320, 600)];
-    [scroller setContentOffset:CGPointMake(0,50) animated:NO];
+
     alertView = [[UIAlertView alloc] initWithTitle:@"" message:@"" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
     isSuccess = NO;
     responseData = [[NSMutableData alloc] init];
-	// Do any additional setup after loading the view.
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(dismissKeyboard)];
+    
+    [self.tableView addGestureRecognizer:tap];
+}
+
+-(void) dismissKeyboard
+{
+    [username_field resignFirstResponder];
+    [email_field resignFirstResponder];
+    [password_field resignFirstResponder];
+    [confirmPassword_field resignFirstResponder];
+    [firstName_field resignFirstResponder];
+    [lastName_field resignFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,18 +58,223 @@
     // Dispose of any resources that can be recreated.
 }
 
--(BOOL) textFieldShouldReturn:(UITextField *)textField
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    [textField resignFirstResponder];
-    [scroller setContentOffset:CGPointMake(0,0) animated:YES];
+    if([textField isEqual:firstName_field])
+    {
+        if([lastName_field.text isEqualToString:@""])
+        {
+            [lastName_field becomeFirstResponder];
+        }
+        else
+        {
+            [firstName_field resignFirstResponder];
+        }
+    }
+    else if([textField isEqual:lastName_field])
+    {
+        if([email_field.text isEqualToString:@""])
+        {
+            [email_field becomeFirstResponder];
+        }
+        else
+        {
+            [lastName_field resignFirstResponder];
+        }
+    }
+    else if([textField isEqual:email_field])
+    {
+        if([username_field.text isEqualToString:@""])
+        {
+            [username_field becomeFirstResponder];
+        }
+        else
+        {
+            [email_field resignFirstResponder];
+        }
+    }
+    else if([textField isEqual:username_field])
+    {
+        if([password_field.text isEqualToString:@""])
+        {
+            [password_field becomeFirstResponder];
+        }
+        else
+        {
+            [username_field resignFirstResponder];
+        }
+    }
+    else if([textField isEqual:password_field])
+    {
+        if([confirmPassword_field.text isEqualToString:@""])
+        {
+            [confirmPassword_field becomeFirstResponder];
+        }
+        else
+        {
+            [password_field resignFirstResponder];
+        }
+    }
+    else if([textField isEqual:confirmPassword_field])
+    {
+        [confirmPassword_field resignFirstResponder];
+    }
     return YES;
 }
 
--(void)textFieldDidBeginEditing:(UITextField *)textField
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    [scroller setContentOffset:CGPointMake(0,textField.center.y-140) animated:YES];
+    return 1;
 }
-- (IBAction)createAccount:(id)sender {
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 9;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell;
+    if(indexPath.row == 0 || indexPath.row == 8)//White Space Cell
+    {
+        static NSString *CellIdentifier = @"WhiteSpaceCell";
+        cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    }
+    else if(indexPath.row == 7) //Button
+    {
+        static NSString *CellIdentifier = @"SignUpCell";
+        cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        signupButton = (UIButton*) [cell viewWithTag:1];
+        [signupButton addTarget:self action:@selector(createAccount:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    else
+    {
+        static NSString *CellIdentifier = @"FieldCell";
+        cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        UILabel* field_label = (UILabel*)[cell viewWithTag:1];
+        if(indexPath.row == 1) //First Name
+        {
+            field_label.text = @"First Name";
+            firstName_field = (UITextField*)[cell viewWithTag:2];
+            firstName_field.placeholder = field_label.text;
+            firstName_field.delegate = self;
+            firstName_field.returnKeyType = UIReturnKeyNext;
+        }
+        else if(indexPath.row == 2) //Last Name
+        {
+            field_label.text = @"Last Name";
+            lastName_field = (UITextField*)[cell viewWithTag:2];
+            lastName_field.placeholder = field_label.text;
+            lastName_field.delegate = self;
+            lastName_field.returnKeyType = UIReturnKeyNext;
+        }
+        else if(indexPath.row == 3) //Email
+        {
+            field_label.text = @"Email";
+            email_field = (UITextField*)[cell viewWithTag:2];
+            email_field.placeholder = field_label.text;
+            email_field.delegate = self;
+            email_field.returnKeyType = UIReturnKeyNext;
+        }
+        else if(indexPath.row == 4) //Username
+        {
+            field_label.text = @"Username";
+            username_field = (UITextField*)[cell viewWithTag:2];
+            username_field.placeholder = field_label.text;
+            username_field.delegate = self;
+            username_field.returnKeyType = UIReturnKeyNext;
+        }
+        else if(indexPath.row == 5) //Password
+        {
+            field_label.text = @"Password";
+            password_field = (UITextField*)[cell viewWithTag:2];
+            password_field.placeholder = field_label.text;
+            password_field.delegate = self;
+            password_field.secureTextEntry = YES;
+            password_field.returnKeyType = UIReturnKeyNext;
+        }
+        else if(indexPath.row == 6) //Password Confirmation
+        {
+            field_label.text = @"Confirmation Password";
+            confirmPassword_field = (UITextField*)[cell viewWithTag:2];
+            confirmPassword_field.placeholder = field_label.text;
+            confirmPassword_field.delegate = self;
+            confirmPassword_field.secureTextEntry = YES;
+            confirmPassword_field.returnKeyType = UIReturnKeyDone;
+        }
+    }
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.row == 0 || indexPath.row == 8)
+    {
+        return 30;
+    }
+    else if(indexPath.row == 7)
+    {
+        return 60;
+    }
+    return 95;
+        
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+- (IBAction)createAccount:(id) sender{
     [self processAccountCreationProcess];
 }
 
@@ -67,18 +283,18 @@
     NSError *error;
     if([self validate_username:username_field.text error:&error]
        && [self validate_email:email_field.text error:&error]
-        && [self validate_firstname:firstName_field.text error:&error]
-         && [self validate_lastname:lastName_field.text error:&error]
-          && [self validate_password:password_field.text confirm:cornfirmPassword_field.text error:&error]
+       && [self validate_firstname:firstName_field.text error:&error]
+       && [self validate_lastname:lastName_field.text error:&error]
+       && [self validate_password:password_field.text confirm:confirmPassword_field.text error:&error]
        )
     {
         NSMutableDictionary* dict = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
-                                        firstName_field.text, @"firstname",
-                                        lastName_field.text, @"lastname",
-                                        email_field.text, @"email",
-                                        username_field.text, @"username",
-                                        password_field.text, @"password",
-                                        nil];
+                                     firstName_field.text, @"firstname",
+                                     lastName_field.text, @"lastname",
+                                     email_field.text, @"email",
+                                     username_field.text, @"username",
+                                     password_field.text, @"password",
+                                     nil];
         NSError* error;
         NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:
                                         [NSURL URLWithString:[[FaceRecServer Server] goToURL:@"/signup"]]];
@@ -169,14 +385,14 @@
             [alertView setMessage:@"Unknown Error"];
             [alertView show];
         }
-
+        
     }
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
     // The request has failed for some reason!
     // Check the error var
-
+    
 }
 
 - (void) connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
